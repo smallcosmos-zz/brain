a61fbbce6ccb2ebf7dbd055562290a1e9d4d59cf
zhaoli <1191599851@qq.com>
2021-04-07 21:31:04 +0800

    完善游戏记录模块

--- src/main/java/edu/tyut/wrx/brain/service/impl/RecordServiceImpl.java
+++ src/main/java/edu/tyut/wrx/brain/service/impl/RecordServiceImpl.java
@@ -2,6 +2,7 @@ package edu.tyut.wrx.brain.service.impl;
 
 import edu.tyut.wrx.brain.dao.RecordMapper;
 import edu.tyut.wrx.brain.model.Record;
+import edu.tyut.wrx.brain.model.ResultCode;
 import edu.tyut.wrx.brain.model.ResultVO;
 import edu.tyut.wrx.brain.service.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,22 +23,22 @@ public class RecordServiceImpl implements RecordService {
     @Override
     public ResultVO addRecordToDb(Record record) {
         if(null == record.getUserId() || StringUtils.isEmpty(record.getUserId().trim())) {
-            return ResultVO.fail("就诊卡号不能为空");
+            return ResultVO.fail(ResultCode.VALIDATION_FAILD_CODE,"就诊卡号不能为空");
         }
         if(null == record.getOrgId()) {
-            return ResultVO.fail("机构id不能空");
+            return ResultVO.fail(ResultCode.VALIDATION_FAILD_CODE,"机构id不能空");
         }
         if(null == record.getGameId()) {
-            return ResultVO.fail("游戏id不能为空");
+            return ResultVO.fail(ResultCode.VALIDATION_FAILD_CODE,"游戏id不能为空");
         }
         if(null == record.getStartTime()) {
-            return ResultVO.fail("游戏开始时间不能为空");
+            return ResultVO.fail(ResultCode.VALIDATION_FAILD_CODE,"游戏开始时间不能为空");
         }
         if(null == record.getEndTime()) {
-            return ResultVO.fail("游戏结束时间不能为空");
+            return ResultVO.fail(ResultCode.VALIDATION_FAILD_CODE,"游戏结束时间不能为空");
         }
         if(null == record.getScore()) {
-            return ResultVO.fail("游戏得分不能为空");
+            return ResultVO.fail(ResultCode.VALIDATION_FAILD_CODE,"游戏得分不能为空");
         }
         //插入之前进行校验，如果已经存在该患者的这个游戏记录则先进行删除
         Record dbRecord = recordMapper.selectRecord(record);
