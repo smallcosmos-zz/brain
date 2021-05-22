cc6821e3234fb92324769dfcd27f9225f4b58ecf
zhaoli <1191599851@qq.com>
2021-05-22 22:34:16 +0800

    查询记录新增错误次数、正确次数、时间

---
+++ src/main/java/edu/tyut/wrx/brain/model/Details.java
@@ -0,0 +1,8 @@
+package edu.tyut.wrx.brain.model;
+
+public class Details {
+    private int correctNum;
+    private int errorNum;
+    private String time;
+
+}
--- src/main/java/edu/tyut/wrx/brain/model/gameAndScore.java
+++ src/main/java/edu/tyut/wrx/brain/model/gameAndScore.java
@@ -3,13 +3,19 @@ package edu.tyut.wrx.brain.model;
 public class gameAndScore {
     private String gameName;
     private Integer score;
+    private Integer correctNum;
+    private Integer errorNum;
+    private Long gameTime;
 
     public gameAndScore() {
     }
 
-    public gameAndScore(String gameName, Integer score) {
+    public gameAndScore(String gameName, Integer score, Integer correctNum, Integer errorNum, Long gameTime) {
         this.gameName = gameName;
         this.score = score;
+        this.correctNum = correctNum;
+        this.errorNum = errorNum;
+        this.gameTime = gameTime;
     }
 
     public String getGameName() {
@@ -28,11 +34,39 @@ public class gameAndScore {
         this.score = score;
     }
 
+    public Integer getCorrectNum() {
+        return correctNum;
+    }
+
+    public void setCorrectNum(Integer correctNum) {
+        this.correctNum = correctNum;
+    }
+
+    public Integer getErrorNum() {
+        return errorNum;
+    }
+
+    public void setErrorNum(Integer errorNum) {
+        this.errorNum = errorNum;
+    }
+
+    public Long getGameTime() {
+        return gameTime;
+    }
+
+    public void setGameTime(Long gameTime) {
+        this.gameTime = gameTime;
+    }
+
+
     @Override
     public String toString() {
         return "gameAndScore{" +
                 "gameName='" + gameName + '\'' +
                 ", score=" + score +
+                ", correctNum=" + correctNum +
+                ", errorNum=" + errorNum +
+                ", gameTime=" + gameTime +
                 '}';
     }
 }
